fbc6f86556fa856ced0637cfce453a7bb1f5401b
Federica <siasfederica@gmail.com>
2020-05-30 12:58:53 +0200

    Riempimento istogramma senza condizioni

--- src/GeneraTree.C
+++ src/GeneraTree.C
@@ -13,8 +13,8 @@
 #include "hit.h"
 #include "Utility.h"
 
-#define ARRAY_SIZE 100 
-#define NUMBER_OF_EVENTS 100
+#define ARRAY_SIZE 100
+#define NUMBER_OF_EVENTS 10000
 
 void GeneraTree() {
 	bool multScattering = false;
@@ -24,34 +24,34 @@ void GeneraTree() {
 	
 	//numero di conteggi per ciascun cilindro
 	hit hits1(0, 0, 0), 
-		  hits2(0, 0, 0),
-		  hitBP(0, 0, 0);
+			hits2(0, 0, 0),
+			hitBP(0, 0, 0);
 	//Dati di partenza: dimensioni dei rivelatori
 	Cilindro  beampipe(0, 3, 0.08, 27), 
-	          det1(1, 4, 0.02, 27), 
-			      det2(2, 7, 0.02, 27); 
+			det1(1, 4, 0.02, 27), 
+			det2(2, 7, 0.02, 27); 
 
 
-  	// Apertura del file di output
-  	TFile hfile("htree.root","RECREATE");
+		// Apertura del file di output
+	TFile hfile("htree.root","RECREATE");
 
-  	// dichiarazione del TTree
+		// dichiarazione del TTree
 	std::string n = "4"; //numero branches del TTree
 	if(multScattering){
 		n = "6"; 
 	}
 
-	TTree *tree = new TTree("T", ("TTree con " + n + " branches").c_str()); //puntatore ad un oggetto ttree
+	TTree *tree = new TTree("Tree", ("TTree con " + n + " branches").c_str()); //puntatore ad un oggetto ttree
 
 	ClonesArray particles("Particella", ARRAY_SIZE); 
 	ClonesArray hit_det1("hit", ARRAY_SIZE);
 	ClonesArray hit_det2("hit", ARRAY_SIZE);
-  //Utili solo in caso di Multiple Scattering
+	//Utili solo in caso di Multiple Scattering
 
 	ClonesArray scatter_det1("Particella", ARRAY_SIZE);
 	ClonesArray scatter_bp("Particella", ARRAY_SIZE);
 
-  // Dichiarazione dei branch del TTree ____________________________________________________________________
+	// Dichiarazione dei branch del TTree ____________________________________________________________________
 	tree->Branch("VertMult", &point.X, "X/D:Y:Z:mult/I");
 	tree->Branch("DirParticella", &particles.ptr);//devo fare .ptr perchè devo accedere al ptr nella classe
 	tree->Branch("hit1", &hit_det1.ptr);
@@ -59,80 +59,80 @@ void GeneraTree() {
 
 	if(multScattering){ 
 
-    tree->Branch("scattering_bp", &scatter_bp.ptr);
-    tree->Branch("scattering_det1", &scatter_det1.ptr);
+		tree->Branch("scattering_bp", &scatter_bp.ptr);
+		tree->Branch("scattering_det1", &scatter_det1.ptr);
 		}
-  
+	
 
-  //INIZIO SIMULAZIONE______________________________________________________________________________________
+	//INIZIO SIMULAZIONE______________________________________________________________________________________
 	for(int i = 0; i < NUMBER_OF_EVENTS; i++) { // loop sugli eventi
-    /*Generazione di un vertice casuale:
-    dispersione su z di alcuni centimetri, in x e y dell’ordine del decimo di millimetro */
+		/*Generazione di un vertice casuale:
+		dispersione su z di alcuni centimetri, in x e y dell’ordine del decimo di millimetro */
 		point.X = gRandom->Gaus(0, 0.01);
 		point.Y = gRandom->Gaus(0, 0.01);
 		point.Z = gRandom->Gaus(0, 5.3);
 
-    point.mult = getMultiplicity(); //molteplicità dell'evento, generata dalla distribuzione data
-
+			point.mult = getMultiplicity(); //molteplicità dell'evento, generata dalla distribuzione data
 
 		cout << "Evento #" << i << "----" << 
-				    "molteplicità = " << point.mult << endl;
+			"molteplicità = " << point.mult << endl;
 
-    /*Conteggio delle interazioni con i rivelatori e con la beampipe*/
+		/*Conteggio delle interazioni con i rivelatori e con la beampipe*/
 		int count_hit1 = 0, count_hit2 = 0, count_bp = 0, count_det1 = 0;
 
 		/*Loop per ogni evento*/
 		for(int j = 0; j < point.mult; j++) {
-		  Vertex vtx_hit = point;
-      /*Metto nell'array particles, la particella j-esima e mi creo una copia di essa in direction*/
+			Vertex vtx_hit = point;
+				/*Metto nell'array particles, la particella j-esima e mi creo una copia di essa in direction*/
 
+			Particella direction(*new(particles.array[j]) Particella(j));
+			cout  << "\nDirezione_Particella #" << direction.getLabel() <<
+			"(event " << i << ") " <<
+			": phi = " << direction.getPhi() <<
+			"; theta = " << direction.getTheta() << endl;
 
-		  Particella direction(*new(particles.array[j]) Particella(j));
-			cout  << "\nDirezione_Particella # " << direction.getLabel()<<
-			    i  << ": phi = " << direction.getPhi()
-				    << ": theta = " << direction.getTheta() << endl;
 			
 			if(multScattering){
-        /*Considero l'interazione con la beampipe solo in caso di Multiple Scattering*/
+				/*Considero l'interazione con la beampipe solo in caso di Multiple Scattering*/
 				hitBP.traject_intersection(point, beampipe, direction);
 
 				/*Se il punto di intersezione su BP soddisfa accettanza non si riempe nulla: 
-        non ci sono HIT su nessuno dei due rivelatori*/
+				non ci sono HIT su nessuno dei due rivelatori*/
 				if(hitBP.acceptance(beampipe)){
 					direction.scattering();//cambio angoli in seguito a MS
-          cout << "newtheta:" << direction.getTheta() << "\nnewPhi:" <<direction.getPhi() <<endl;
-          change_vertex(vtx_hit, hitBP); //prende l'intersezione come nuovo vertice
-          //crea particella che è copia di direction e la salva nell'array
-          new(scatter_bp.array[count_bp]) Particella(direction);
-          count_bp++;
+					cout << "newtheta:" << direction.getTheta() << "\nnewPhi:" <<direction.getPhi() <<endl;
+					change_vertex(vtx_hit, hitBP); //prende l'intersezione come nuovo vertice
+					//crea particella che è copia di direction e la salva nell'array
+					new(scatter_bp.array[count_bp]) Particella(direction);
+					count_bp++;
 
-          /*Interazione con il primo rivelatore*/
+					/*Interazione con il primo rivelatore*/
 					hits1.traject_intersection(vtx_hit, det1, direction);
-				  /*Se il punto di intersezione sul primo rivelatore soddisfa l'accettanza si riempe hit_det1.
-          Se non è dentro al primo non può essere nel secondo: man mano che si allontana dal
-          vertice è sempre più esterno come punto su asse z*/
+					/*Se il punto di intersezione sul primo rivelatore soddisfa l'accettanza si riempe hit_det1.
+					Se non è dentro al primo non può essere nel secondo: man mano che si allontana dal
+					vertice è sempre più esterno come punto su asse z*/
 
 					if(hits1.acceptance(det1)){
 						cout <<"Z1:"<<endl;
-            hits1.PrintStatus();
+						hits1.PrintStatus();
 						direction.scattering(); //modifica la direzione della particella in seguito al MS
-            change_vertex(vtx_hit, hits1); //prende l'intersezione come nuovo vertice
-            new(scatter_det1.array[count_det1]) Particella(direction);
+						change_vertex(vtx_hit, hits1); //prende l'intersezione come nuovo vertice
+						new(scatter_det1.array[count_det1]) Particella(direction);
 						count_det1++;
 
-            //Smearing sul primo rivelatore
-            smearing(hits1, det1);
-            new(hit_det1.array[count_hit1]) hit(hits1) ;
-            count_hit1++;
+						//Smearing sul primo rivelatore
+						smearing(hits1, det1);
+						new(hit_det1.array[count_hit1]) hit(hits1) ;
+						count_hit1++;
 
-					hits2.traject_intersection(vtx_hit, det2, direction);
-          if(hits2.acceptance(det2)){
-					  cout <<"Z2:" <<endl;
-						hits2.PrintStatus();
+						hits2.traject_intersection(vtx_hit, det2, direction);
+						if(hits2.acceptance(det2)){
+							cout <<"Z2:" <<endl;
+							hits2.PrintStatus();
 
-						smearing(hits2, det2);
-						new(hit_det2.array[count_hit2])hit(hits2);
-						count_hit2++;
+							smearing(hits2, det2);
+							new(hit_det2.array[count_hit2])hit(hits2);
+							count_hit2++;
 						}
 
 					}
@@ -140,27 +140,27 @@ void GeneraTree() {
 			}
 
 		/* MS è FALSE si passa direttamente qui dove si effettuare prima l'intersezione 
-    sul primo layer, si verifica accettanza,si valuta smearing e si riempe hit_det1.
+		sul primo layer, si verifica accettanza,si valuta smearing e si riempe hit_det1.
 		Se si è passato il primo rivelatore si passerà poi al secondo e si controlla
-    accettanza, smearing e si riempe hit_det2*/
+		accettanza, smearing e si riempe hit_det2*/
 
 			else {
 
 				hits1.traject_intersection(vtx_hit, det1, direction);
-        if(hits1.acceptance(det1)){
-          cout <<"Z1:"<<endl;
+				if(hits1.acceptance(det1)){
+					cout <<"Z1:"<<endl;
 					hits1.PrintStatus();
-          smearing(hits1, det1);
+					smearing(hits1, det1);
 					new(hit_det1.array[count_hit1]) hit(hits1);
 					count_hit1++;
 
-          /*Interazione con rivelatore 2*/
+					/*Interazione con rivelatore 2*/
 					hits2.traject_intersection(vtx_hit, det2, direction);
 					if(hits2.acceptance(det2)){
 						cout <<"Z2:" <<endl;
 						hits2.PrintStatus();
 
-					  smearing(hits2, det2);
+						smearing(hits2, det2);
 						new(hit_det2.array[count_hit2])hit(hits2);
 						count_hit2++;
 					}
@@ -175,15 +175,15 @@ void GeneraTree() {
 		}
 
 		/*RUMORE: punti aggiuntivi che si mettono per ogni evento simulato oltre ai conteggi veri. 
-    Sono conteggi spuri dovuti all'elettronica.Se non dovessi soddisfare le condizioni di accettanza
-    del primo detector(senza MS) o della BP(con MS) si salverebbero solo i conteggi di noise che sono
-    messi dentro al ciclo degli eventi.*/
+		Sono conteggi spuri dovuti all'elettronica.Se non dovessi soddisfare le condizioni di accettanza
+		del primo detector(senza MS) o della BP(con MS) si salverebbero solo i conteggi di noise che sono
+		messi dentro al ciclo degli eventi.*/
 
 		
-		  if(noise){
-        add_noise(hits1, det1, count_hit1, hit_det1); 
-        add_noise(hits2, det2, count_hit2, hit_det2);
-		  }
+			if(noise){
+				add_noise(hits1, det1, count_hit1, hit_det1); 
+				add_noise(hits2, det2, count_hit2, hit_det2);
+			}
 
 
 		tree->Fill();
--- src/LeggiTree.C
+++ src/LeggiTree.C
@@ -12,36 +12,37 @@
 #include "Utility.h"
 #include "TCanvas.h"
 #include "TH1D.h"
+#include <algorithm>
+#include <vector>
 
 #define ARRAY_SIZE 100
 #define DELTAPHI 0.006 //6 mrad differenza di phi oltre la quale i due hit non possono appartenere allo stesso vertice
 
-void LeggiTree(){
+void LeggiTree() {
+  //Dati di partenza: dimensioni dei rivelatori
+  Cilindro beampipe(0, 3, 0.08, 27),
+          det1(1, 4, 0.02, 27),
+          det2(2, 7, 0.02, 27);
+  //Dichiarazione NTupla e file in cui salvare i vertici
+  TFile fout("vtxreco.root","recreate");
+  TNtuple *nt = new TNtuple("nt","vertices","zsim:zrec:diff");
 
-//Dati di partenza: dimensioni dei rivelatori
- Cilindro  beampipe(0, 3, 0.08, 27),
-		         det1(1, 4, 0.02, 27),
-				 	 det2(2, 7, 0.02, 27);
-//Dichiarazione NTupla e file in cui salvare i vertici
- TFile fout("vtxreco.root","recreate");
- TNtuple *nt = new TNtuple("nt","vertices","zsim:zrec");
- float znt[2];
+  //Dichiarazione TClonesArray
+  static Vertex point;
+  ClonesArray particles("Particella", ARRAY_SIZE);
+  ClonesArray hit_det1("hit", ARRAY_SIZE);
+  ClonesArray hit_det2("hit", ARRAY_SIZE);
 
-//Dichiarazione TClonesArray
- static Vertex point;
- ClonesArray particles("Particella", ARRAY_SIZE);
- ClonesArray hit_det1("hit", ARRAY_SIZE);
- ClonesArray hit_det2("hit", ARRAY_SIZE);
+  //Apertura file di input
+  TFile hfile("htree.root");
+  //Lettura TTree  e branch
+  TTree *tree = (TTree*) hfile.Get("Tree;1");
 
-//Apertura file di input
- TFile hfile("htree.root");
-//Lettura TTree  e branch
- TTree *tree = (TTree*)hfile.Get("T;1");
-//Definizione dei branch e degli indirizzi per la lettura dei dati su ttree
- tree->SetBranchAddress("VertMult",&point.X);
- tree->SetBranchAddress("DirParticella",&particles.ptr);
- tree->SetBranchAddress("hit1",&hit_det1.ptr);
- tree->SetBranchAddress("hit2",&hit_det2.ptr);
+  //Definizione dei branch e degli indirizzi per la lettura dei dati su ttree
+  tree->SetBranchAddress("VertMult",&point.X);
+  tree->SetBranchAddress("DirParticella",&particles.ptr);
+  tree->SetBranchAddress("hit1",&hit_det1.ptr);
+  tree->SetBranchAddress("hit2",&hit_det2.ptr);
 
  /*TBranch *b1=tree->GetBranch("VertMult");
  TBranch *b2=tree->GetBranch("DirParticella");
@@ -54,48 +55,139 @@ void LeggiTree(){
  b4->SetAddress(&hit_smear2.ptr);*/
 
 //varibili utili alla ricostruzione
- int evsim=tree->GetEntries();
- int entries1 = 0, entries2 = 0;
- double deltaR=(det2.getRadius()- det1.getRadius());
- double deltaphi;
- //TH1D *z = new TH1D("z","istogramma ",15,-10.5,10.5);
- //z->SetDirectory(0);
- for(int i=0;i<evsim;i++){
-	 tree->GetEvent(i);
-	 entries1=hit_det1.ptr->GetEntries();
-	 entries2=hit_det2.ptr->GetEntries();
-	 //cout<<"evento "<<i<<endl;
-	 //cout<<"Entries-hit1: "<<entries1<<endl;
-	 //cout<<"Entries-hit2: "<<entries2<<endl;
-	 cout<<"vtx: "<<point.Z<<endl;
-	 znt[0]=point.Z;
-//definisco un vettore di vertici in cui raccogliere i vertici ricostruiti
-	 Vertex rec_vtx[entries1*entries2];
-	 int count = 0;
+  int evsim=tree->GetEntries();
+  int entries1 = 0, entries2 = 0;
+  double deltaR=(det2.getRadius()- det1.getRadius());
+  double deltaphi;
+  //TH1D *z = new TH1D("z","istogramma ",15,-10.5,10.5);
+  //z->SetDirectory(0);
+  for(int i=0 ;i < evsim; i++) {
+    tree->GetEvent(i);
+    entries1=hit_det1.ptr->GetEntries();
+    entries2=hit_det2.ptr->GetEntries();
+    //cout<<"evento "<<i<<endl;
+    //cout<<"Entries-hit1: "<<entries1<<endl;
+    //cout<<"Entries-hit2: "<<entries2<<endl;
+    //cout<<"vtx: "<<point.Z<<endl;
 
-//ciclo per verificare quali punti rimandano ad un vertice,partendo dagli hit sul più esterno
-//prendo contenuto della cella j-esima e dell'i esima e controllo la differenza in phi per capire se possono appartenere alla stessa traiettoria
-	 for(int j=0;j<entries2;j++){
-		 hit *hit2_event=(hit*)hit_det2.ptr->At(j);
-		 for(int k=0;k<entries1;k++){
-			 hit *hit1_event=(hit*)hit_det1.ptr->At(j);
-			 deltaphi=abs((hit2_event->getPhi()-hit1_event->getPhi()));
-		//determino il vertice come intersezione della retta passante per i due hit con l asse del fascio(z)
-			 if(deltaphi<DELTAPHI){
-				 reconstruction_vtx(rec_vtx[count],*hit1_event,*hit2_event,det1,deltaR);
-				 cout<<"vtx ricostruito: "<<rec_vtx[count].Z<<endl;
-				 count++;
-			 }
-		 }
-//MANCA IL CALCOLO DEL MASSIMO-tracklets DEI VARI VALORI DI Zrec, PER RIEMPIRE L'NTUPLA HO USATO L'ULTIMO VALORE DI Z RICOSTRUITO
- //PRIMA DI PASSARE ALL'EVENTO SUCCESSIVO
-		 znt[1]=rec_vtx[count].Z;
-	 }
+    //definisco un vettore di vertici in cui raccogliere i vertici ricostruiti
+    Vertex rec_vtx[entries1*entries2];
+    int count = 0;
 
-	 nt->Fill(znt);
- }
+    //ciclo per verificare quali punti rimandano ad un vertice,partendo dagli hit sul più esterno
+    //prendo contenuto della cella j-esima e dell'i esima e controllo la differenza in phi per capire se possono appartenere alla stessa traiettoria
+    for(int j=0;j<entries2;j++) {
+      hit *hit2_event = (hit*) hit_det2.ptr->At(j);
+      for(int k=0;k<entries1;k++){
+        hit *hit1_event = (hit*) hit_det1.ptr->At(k);
+        deltaphi = abs((hit2_event->getPhi() - hit1_event->getPhi()));
+        //determino il vertice come intersezione della retta passante per i due hit con l asse del fascio(z)
+        if(deltaphi<DELTAPHI){
+          reconstruction_vtx(rec_vtx[count],*hit1_event,*hit2_event,det1,deltaR);
+          //cout<<"vtx ricostruito: "<<rec_vtx[count].Z<<endl;
+          count++;
+        }
+      }
+    }
+    //MANCA IL CALCOLO DEL MASSIMO-tracklets DEI VARI VALORI DI Zrec, PER RIEMPIRE L'NTUPLA HO USATO L'ULTIMO VALORE DI Z RICOSTRUITO
+    //PRIMA DI PASSARE ALL'EVENTO SUCCESSIVO
+        
+    double width = 0.1;
+    double bin_extreme = 200;//det2.getRadius()*2*100;
+    double nbin=(2*bin_extreme)/width;
+    int binmax=0, nc = 0;
+    double most_prob_Z = 0;
+    bool twopeaks = false;
 
-hfile.Close();
-fout.Write();
-fout.Close();
+    TH1D *trackZ = new TH1D("VertrecZ","tracklets",nbin+1,-bin_extreme-(width/2.),bin_extreme+(width/2.));
+    for(int i=0;i<count;i++){	
+      trackZ->Fill(rec_vtx[i].Z);
+      TAxis *xaxis = trackZ->GetXaxis();
+      Int_t binx = xaxis->FindBin(rec_vtx[i].Z);
+      //cout<<"vtx ricostruito: "<<rec_vtx[i].Z << "pos:"<<binx<<endl;
+    }
+
+    binmax = trackZ->GetMaximumBin();
+    
+    for(int v=0;v<nbin+1;v++){
+      if(trackZ->GetBinContent(v) == trackZ->GetBinContent(binmax) && v!=binmax && abs(v-binmax) >= 10) {
+        bin_extreme = std::max(v, binmax); 
+        twopeaks = true; 
+      }
+    }
+
+    if (twopeaks) { 
+      delete trackZ;
+      trackZ = new TH1D("VertrecZ","tracklets",nbin+1,-bin_extreme-(width/2.),bin_extreme+(width/2.));    
+      twopeaks = false; 
+
+      for(int i=0;i<count;i++)
+        trackZ->Fill(rec_vtx[i].Z);
+      
+      binmax = trackZ->GetMaximumBin();
+
+      for(int v=0;v<nbin+1;v++){
+        if(trackZ->GetBinContent(v) == trackZ->GetBinContent(binmax) && v!=binmax && abs(v-binmax) >= 10) {
+          bin_extreme = std::max(v, binmax); 
+          twopeaks = true; 
+        }
+      }    
+    }
+
+  /*
+    //Controllo se ci sono più picchi
+    for(int v=0;v<nbin+1;v++){
+      if(trackZ->GetBinContent(v)==trackZ->GetBinContent(binmax)&&v!=binmax){
+        if(abs(v-binmax)>=10){
+          if(v<binmax){
+            bin_extreme=binmax+20;
+            cout <<" REBIN"<< endl;
+            delete trackZ;
+            trackZ = new TH1D("VertrecZ","tracklets",nbin+1,-bin_extreme-(width/2.),bin_extreme+(width/2.));
+            //trackZ =  TH1D("VertrecZ","tracklets",nbin+1,-bin_extreme-(width/2.),bin_extreme+(width/2.));
+              for(int i=0;i<count;i++){	
+                trackZ->Fill(rec_vtx[i].Z);
+                TAxis *xaxis = trackZ->GetXaxis();
+                Int_t binx = xaxis->FindBin(rec_vtx[i].Z);
+                cout<<"vtx ricostruito: "<<rec_vtx[i].Z << "pos:"<<binx<<endl;
+              }
+              for(int v=0;v<nbin+1;v++){
+                if(trackZ->GetBinContent(v)==trackZ->GetBinContent(binmax)&&v!=binmax){
+                  twopeaks=true;
+                  }
+                }
+            }
+          }
+        }
+      }*/
+    
+
+    //Determino valore più probabile facendo la media pesata intorno al max
+    
+    //binmax = trackZ->GetMaximumBin();
+    //most_prob_Z = trackZ->GetBinCenter(binmax);
+    //nc = trackZ->GetBinContent(binmax);
+    if(twopeaks == false) {
+      double sum=0;
+      double ncounts=0;
+      for(int i=binmax-3; i<binmax+4; i++) {
+        sum += trackZ->GetBinContent(i)*trackZ->GetBinCenter(i);
+        ncounts += trackZ->GetBinContent(i);
+      }
+
+      most_prob_Z = sum/ncounts;
+      cout << "vtx originale:"<< point.Z << " -- vtx ricostruito: "<< most_prob_Z <<endl;
+    //QUESTO NON VA BENE
+      if(abs(point.Z - most_prob_Z)<1)
+        nt->Fill(point.Z, most_prob_Z,  (point.Z - most_prob_Z));
+    }
+    
+    //nt->Fill(point.Z, most_prob_Z, point.Z -most_prob_Z);
+
+    delete trackZ;
+  }
+
+
+  fout.Write();
+  fout.Close();
 }
